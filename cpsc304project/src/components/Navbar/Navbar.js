2be78264e02fc53cf5cfc9c5785cd70d82959062
Vicki <s6p1j@students.cs.ubc.ca>
2023-07-24 00:33:33 -0700

    nav

--- cpsc304project/src/components/Navbar/Navbar.js
+++ cpsc304project/src/components/Navbar/Navbar.js
@@ -2,16 +2,20 @@ import './Navbar.css'
 
 export default function Navbar(){
     return <nav className="nav">
-        <ul>
-            <a href="/" className="site-title">NOTBNB</a>
-        </ul>
-        <ul className="nav-options">
-            <li>
-                <a href="/services">SERVICES</a>
-            </li>
-            <li>
-                <a href="/login">LOGIN / SIGN UP</a>
-            </li>
-        </ul>
+        <div className="container">
+            <div>
+                <a href="/" className="site-title">NOTBNB</a>
+            </div>
+            <div>
+                <ul className="nav-options">
+                    <li>
+                        <a href="/services">SERVICES</a>
+                    </li>
+                    <li>
+                        <a href="/login">LOGIN / SIGN UP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 }
