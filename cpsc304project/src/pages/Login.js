5de6722c5ac94e57a471dfc4916723e6a1e94fe5
Vicki <s6p1j@students.cs.ubc.ca>
2023-07-25 22:11:26 -0700

    routing

--- cpsc304project/src/pages/Login.js
+++ cpsc304project/src/pages/Login.js
@@ -1,7 +1,13 @@
 import Navbar from '../components/Navbar/Navbar';
+import './Login.css'
+
 
 export default function Login(){
     return <>
-        <Navbar/>
-    </>
+            <div className='navbar'><Navbar/></div>
+            
+            <div className="login-container">
+                <h1 className='heading'>LOG IN</h1>
+            </div>
+        </>
 }
