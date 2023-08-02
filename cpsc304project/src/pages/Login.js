0dfb1573486cd146a95ceeafc8ac1d9fab71906b
Vicki <s6p1j@students.cs.ubc.ca>
2023-08-01 21:09:25 -0700

    login page

--- cpsc304project/src/pages/Login.js
+++ cpsc304project/src/pages/Login.js
@@ -5,9 +5,26 @@ import './Login.css'
 export default function Login(){
     return <>
             <div className='navbar'><Navbar/></div>
-            
+
             <div className="login-container">
-                <h1 className='heading'>LOG IN</h1>
+                <form action="">
+                    <div className='input-fields'>
+                        <div className='input-login'>
+                            <input type="username" placeholder='Username' className='form-control'>
+                            </input>
+                        </div>
+                        <div className="input-login">
+                            <input type="password" placeholder='Password' className='form-control'>
+                            </input>
+                        </div>
+                    </div>
+                    <button className='button-login'>LOG IN</button>
+                </form>
+            </div>
+            
+            <div className='register-texts'>
+                <h1>Don't have an account?</h1>
+                <a href="signup">Register Now.</a>
             </div>
         </>
 }
