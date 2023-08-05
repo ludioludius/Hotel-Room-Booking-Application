694fe66f4f32f9236fb7aeb6f09d79e113490413
Vicki <s6p1j@students.cs.ubc.ca>
2023-08-05 15:10:28 -0700

    post function wip

--- cpsc304project/src/pages/Post.js
+++ cpsc304project/src/pages/Post.js
@@ -1,12 +1,13 @@
 import Navbar from '../components/Navbar/Navbar';
 import './Post.css'
 import React, { useState }  from 'react'
-
+import axios from 'axios';
 
 
 export default function Post(){
 
     const [selectedButton, setSelectedButton] = useState(null);
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const handleButtonClick = (event) => {
       const button = event.target.textContent;
@@ -17,6 +18,19 @@ export default function Post(){
     function handleSubmit(event) {
         event.preventDefault();
     }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.get(`/api/check-id/${ID}`)
+          .then((response) => {
+            // The response data should indicate if the ID is found or not
+            const isIDFound = response.data.found;
+            setShowDropdown(isIDFound);
+          })
+          .catch((error) => {
+            console.error('Error checking ID:', error);
+          });
+      }
 
     return <>
         <div className='navbar'><Navbar/></div>
@@ -39,8 +53,13 @@ export default function Post(){
                     <input type="id" placeholder='ID' className='form-control'
                     onChange={e => setPosterID(e.target.value)}/>
                 </div>
+                <button className='create-post'>CREATE A LISTING</button>
             </form>
-            <button className='create-post'>CREATE A LISTING</button>
-        </div>
+            {showDropdown && (
+            <div className="dropdown-section">
+                {/* Dropdown content */}
+            </div>
+            )}
+           </div>
     </>
 }
